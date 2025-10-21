02d6ef38d2a79e060ab377cb2e5f313e40251b21
abdallahkaram1994 <abdallah.karam@tripninja.io>
2025-10-21 17:08:44 -0300

    Update Constants.cs

--- src/TN.SDK/Utils/Constants.cs
+++ src/TN.SDK/Utils/Constants.cs
@@ -4,12 +4,6 @@ namespace TN.SDK.Utils;
 
 public static class Constants
 {
-    public static class ApiUrls
-    {
-        public static string SANDBOX_API_URL => Environment.GetEnvironmentVariable("API_SANDBOX_URL") ?? "https://sandbox.tripninja.io";
-
-        public static string PRODUCTION_API_URL => Environment.GetEnvironmentVariable("API_PRODUCTION_URL") ?? "https://api.tripninja.io";
-    }
     public static class CompressionSettings
     {
         public const CompressionLevel DEFAULT_COMPRESSION_LEVEL = CompressionLevel.Optimal;
